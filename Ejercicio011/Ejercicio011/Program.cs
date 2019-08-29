55db1c61a2fb181b62c9705f4e295893bec2ac64
alumno <alumno@utnfra.com.ar>
2019-08-28 21:55:10 -0300

    Ejercicios

---
+++ Ejercicio011/Ejercicio011/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio011
+{
+    class Ejercicio011
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 11";
+
+            int max = 0;
+            int min = 0;
+            int valor;
+            int acumulador = 0;
+            int contador = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write("Ingrese 6 numeros enteros: ");
+                while ( !int.TryParse(Console.ReadLine(), out valor) ||
+                        !Validacion.Validar(valor, -100, 100))
+                {
+                    Console.WriteLine("Ingrese 10 numeros enteros validos: ");
+                }
+
+                acumulador += valor;
+                contador++;
+
+                if (i == 0)
+                {
+                    max = valor;
+                    min = valor;
+                }
+                else if (valor > max)
+                {
+                    max = valor;
+                }
+                else if (valor < min)
+                {
+                    min = valor;
+                }
+            }
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Maximo : {0}", max);
+            Console.WriteLine("Minimo : {0}", min);
+            Console.WriteLine("Promedio : {0}", acumulador / contador);
+            Console.ReadKey();
+        }
+    }
+
+    class Validacion
+    {
+        public static bool Validar(int valor, int min, int max)
+        {
+            if(valor < min || valor > max)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Ejercicio014/Ejercicio014/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio014
+{
+    class Ejercicio014
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 14";
+
+            double ladoCuadrado, baseTriangulo, alturaTriangulo, radioCirculo;
+            double areaCuadrado, areaTriangulo, areaCirculo;
+            int opcionUno;
+
+            Console.Write("Seleccione que calculo desea realizar: \n");
+            Console.Write("1 - Calcular Area del Cuadrado  \n");
+            Console.Write("2 - Calcular Area del Triangulo \n");
+            Console.Write("3 - Calcular Area del Circulo   \n");
+            opcionUno = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcionUno)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine("AREA CUADRADO\n");
+                    Console.Write("Ingrese lado del cuadrado: ");
+                    ladoCuadrado = Convert.ToInt32(Console.ReadLine());
+                    areaCuadrado = CalculoDeArea.CalcularCuadrado(ladoCuadrado);
+                    Console.WriteLine("Area del cuadrado: {0}", areaCuadrado);
+                    break;
+
+                case 2:
+                    Console.Clear();
+                    Console.WriteLine("\nAREA TRIANGULO\n");
+                    Console.Write("Ingrese base del triangulo: ");
+                    baseTriangulo = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Ingrese altura del triangulo: ");
+                    alturaTriangulo = Convert.ToInt32(Console.ReadLine());
+                    areaTriangulo = CalculoDeArea.CalcularTriangulo(baseTriangulo, alturaTriangulo);
+                    Console.WriteLine("Area del triangulo: {0}", areaTriangulo);
+                    break;
+
+                case 3:
+                    Console.Clear();
+                    Console.WriteLine("\nAREA CIRCULO\n");
+                    Console.Write("Ingrese el radio circulo: ");
+                    radioCirculo = Convert.ToInt32(Console.ReadLine());
+                    areaCirculo = CalculoDeArea.CalcularCirculo(radioCirculo);
+                    Console.WriteLine("Area del circulo: {0}", areaCirculo);
+                    break;
+                
+                default:
+                    Console.Clear();
+                    Console.Write("Opción no válida");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
