c26317c77a575363b1a3114699768719b1b3a609
alumno <alumno@utnfra.com.ar>
2019-08-28 22:06:51 -0300

    Ejercicios

---
+++ Ejercicio015/Ejercicio015/Calculadora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio015
+{
+    class Calculadora
+    {
+        public static float Calcular(float numeroUno, float numeroDos, char operacion)
+        {
+            float calculo;
+            float resultado;
+
+            if(calculo == )
+        }
+    }
+}
---
+++ Ejercicio015/Ejercicio015/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio015
+{
+    class Ejercicio015
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
